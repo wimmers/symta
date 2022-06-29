8df2d80c2fbca675fd43d312bbff40a4013541ab
Simon Wimmer <swimmer@cs.au.dk>
2022-06-29 09:33:40 +0200

    Extended info logging in k-induction

--- bin/BMC.ml
+++ bin/BMC.ml
@@ -74,7 +74,48 @@ module BMC (System: System) (Context: Context) = struct
     | _ -> loop 1
   )
 
-  let k_induction bound = Solver.(
+  let pp_opt pp_value ppf = function
+  | None -> Caml.Format.fprintf ppf "none"
+  | Some x -> pp_value ppf x
+
+  let project_index_vars model index vars =
+    let expr_of e = Model.eval model e true in
+    List.map vars ~f:(fun e -> e, expr_of (index_var_of index e))
+
+  let project_model model bound =
+    let project i =
+      Util.apply3 (pre_vars, aux_vars, post_vars)
+        ~f:(project_index_vars model i)
+    in
+    List.map Util.(0--(bound+1)) ~f:project
+
+  let print_projected interpretations =
+    let open Caml.Format in
+    let ppf = Caml.Format.std_formatter in
+    let pp_interp_pair (e, interp) =
+      fprintf ppf "@[%a:@ %a@]@ " pp_expr e (pp_print_option pp_expr) interp
+    in
+    let pp_interp_pairs description pairs =
+      printf "@[%s:@ @[<v>" description;
+      List.iter pairs ~f:pp_interp_pair;
+      printf "@]@]\n@,";
+    in
+    let init, auxs, _ = List.hd_exn interpretations in
+    pp_interp_pairs "Init" (init @ auxs);
+    List.iteri (List.tl_exn interpretations) ~f:(fun i (_, auxs, posts) ->
+      printf "@,@[@[Step:@ %d@]\n\n@," (i + 1);
+      printf "@[Aux:@ @[<v>";
+      List.iter auxs ~f:pp_interp_pair;
+      printf "@]@]\n@,";
+      printf "@[Post:@ @[<v>";
+      List.iter posts ~f:pp_interp_pair;
+      printf "@]@]";
+      printf "@]@."
+    )
+
+  let k_induction bound =
+    let open Solver in
+    let printf = Caml.Format.printf in
     let mk_direct_simulations i =
       match direct_simulation_opt with
       | Some sim ->
@@ -83,7 +124,13 @@ module BMC (System: System) (Context: Context) = struct
           |> reindex j post_vars
           |> Boolean.mk_not ctxt)
       | None -> [] in
+    let _mk_direct_simulations _i = [Boolean.mk_true ctxt] in
     let solver = mk_simple_solver ctxt in
+    let print_model i = match get_model solver with
+      | None -> printf "Failed to retrieve model"
+      | Some model ->
+        project_model model i |> print_projected
+    in
     let init = reindex 0 invar_vars init in
     let rec loop i neg_preds =
       if i > bound then
@@ -95,20 +142,29 @@ module BMC (System: System) (Context: Context) = struct
         add solver [trans; invar];
         (* Caml.Format.printf "Pred %d: %a@." i pp_expr pred;
         Stdio.print_endline (to_string solver |> normalize_z3_sexps); *)
+        (* printf "New simulations: %a@."
+          pp_expr_comma_list (mk_direct_simulations i); *)
         add solver (mk_direct_simulations i);
         match check solver [init; pred] with
-        | SATISFIABLE -> sprintf "Reaching run of length: %d" i
+        | SATISFIABLE ->
+          printf "Model:\n@.";
+          print_model i;
+          sprintf "Reaching run of length: %d" i;
         | _  ->
           match check solver (pred :: neg_preds) with
           | UNSATISFIABLE -> sprintf "Invariant of diameter: %d" i
-          | _ -> loop (i + 1) (Boolean.mk_not ctxt pred :: neg_preds)
+          | _ ->
+            printf "k-CTI for k = %d:\n@." i;
+            print_model i;
+            loop (i + 1) (Boolean.mk_not ctxt pred :: neg_preds)
     in
     let invar = reindex 0 invar_vars invar in
     let pred = reindex 0 pre_vars pred in
+    printf "@[Simulation:@ %a@]@." (pp_opt pp_expr) direct_simulation_opt;
     add solver [invar];
     match check solver [init; pred] with
     |  SATISFIABLE -> "Initial states satisfy predicate"
     | _ -> loop 1 [Boolean.mk_not ctxt pred]
-  )
+
 
 end
